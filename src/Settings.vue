4b17992d04e35c61a5063498f5de9ab0055ce21d
Terra <bieyuanxi@163.com>
2026-04-17 16:00:21 +0800

    1

--- src/Settings.vue
+++ src/Settings.vue
@@ -1,25 +1,56 @@
 <script setup lang="ts">
-import { NButton, NSpace, NCard } from "naive-ui";
+import { NButton, NSpace, NCard, NRadioGroup, NRadioButton } from "naive-ui";
 
+import { setAppTheme, appTheme } from "./utils/theme";
 
-import { setAppTheme } from "./utils/theme";
+import type { Theme } from "@tauri-apps/api/window";
+import { computed } from "vue";
 
+type ThemeTpye = Theme | "system";
 
+type ThemeOption = {
+    value: ThemeTpye;
+    label: string;
+};
 
+const theme = computed(() => {
+    return null === appTheme.value ? "system" : appTheme.value;
+});
+
+const themeOptions: ThemeOption[] = [
+    {
+        value: "dark",
+        label: "深色",
+    },
+    {
+        value: "light",
+        label: "浅色",
+    },
+    {
+        value: "system",
+        label: "跟随系统",
+    },
+];
 </script>
 
 <template>
     <n-space>
         <n-card>
-            <n-button @click="setAppTheme('dark')">
-                深色
-            </n-button>
-            <n-button @click="setAppTheme('light')">
-                浅色
-            </n-button>
-            <n-button @click="setAppTheme(null)">
-                跟随系统
-            </n-button>
+            <n-radio-group
+                :value="theme"
+                @update:value="
+                    (v: ThemeTpye) => setAppTheme(v === 'system' ? null : v)
+                "
+                name="theme_options"
+            >
+                <n-radio-button
+                    v-for="theme in themeOptions"
+                    :key="theme.value"
+                    :value="theme.value"
+                    :disabled="false"
+                    :label="theme.label"
+                />
+            </n-radio-group>
         </n-card>
         <n-button>Default</n-button>
         <n-button type="tertiary"> Tertiary </n-button>
--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,4 +1,4 @@
-import { ref, watch } from "vue";
+import { readonly, ref, watch } from "vue";
 import { store } from "./store";
 import { info } from "@tauri-apps/plugin-log";
 import { getCurrentWindow } from "@tauri-apps/api/window";
@@ -9,44 +9,48 @@ import { setTheme } from '@tauri-apps/api/app';
 export type AppTheme = Theme | null;
 
 // app theme config
-export const appTheme = ref<AppTheme>(null);
+const _appTheme = ref<AppTheme>(null);
+export const appTheme = readonly(_appTheme);
+
 // real theme of the app
-export const windowTheme = ref<AppTheme>(null);
+const _windowTheme = ref<AppTheme>(null);
+export const windowTheme = readonly(_windowTheme);
 
 /**
  * **make sure call this only once**
  */
 export async function initTheme() {
+  // 监听系统主题
+  await getCurrentWindow().onThemeChanged(({ payload: theme }) => {
+    info("system theme changed: " + theme);
+    _windowTheme.value = theme;
+  });
+  
+  watch(appTheme, async (theme) => {
+    info("set app theme: " + theme);
+    
+    // sync app theme
+    await setTheme(theme);
+    
+    // store theme
+    await store.set("theme", theme);
+    await store.save();
+    info(`saved theme cfg: ${theme}`);
+  })
+  
   // 读取配置
   const savedTheme = await store.get<AppTheme>("theme");
   const useTheme = savedTheme ?? null; // 没有则为跟随系统
   
-  await setAppTheme(useTheme);
+  setAppTheme(useTheme);
   // 启动时同步窗口主题
-  windowTheme.value = await getCurrentWindow().theme();
-  
-  // 监听系统主题
-  await getCurrentWindow().onThemeChanged(({ payload: theme }) => {
-    info("system theme changed: " + theme);
-    windowTheme.value = theme;
-  });
-  
-  watch(appTheme, async (theme) => {
-    // store theme
-    info(`saving theme cfg: ${theme}`);
-    await store.set("theme", theme);
-    await store.save();
-  });
+  _windowTheme.value = await getCurrentWindow().theme();
 }
 
 /**
  * Set theme of the App.
  * @param theme 
  */
-export async function setAppTheme(theme: AppTheme) {
-  appTheme.value = theme;
-  info("set app theme: " + theme);
-  
-  // sync app theme
-  await setTheme(theme);
+export function setAppTheme(theme: AppTheme) {
+  _appTheme.value = theme;
 }
